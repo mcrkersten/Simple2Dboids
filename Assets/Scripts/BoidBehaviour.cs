2b47afb455bebb9ec2747cf13c89d244e875562f
Max <max-kersten@live.nl>
2021-11-21 13:58:20 +0100

    Remove unused script

--- Assets/Scripts/BoidBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BoidBehaviour : ScriptableObject
-{
-    public abstract Vector2 CalculateBoidMovement(BoidAgent agent, List<Transform> context, BoidManager manager);
-}
